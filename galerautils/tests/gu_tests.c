e09a7a1ad0056105a6b092c330114aa1b5a28e93
Otto Kekäläinen <otto@debian.org>
2019-07-25 14:04:04 -0300

    disable-sporadic-failing-tests
        Disable test that apparently has a race condition and sporadically fails.
        Upstream developer Teemu Ollakka confirmed that the failures in this test
        were not a sign of a real bug.
    
    
    Gbp-Pq: Name disable-sporadic-failing-tests.patch

--- galerautils/tests/gu_tests.c
+++ galerautils/tests/gu_tests.c
@@ -20,7 +20,6 @@
 #include "gu_time_test.h"
 #include "gu_fifo_test.h"
 #include "gu_uuid_test.h"
-#include "gu_lock_step_test.h"
 #include "gu_str_test.h"
 #include "gu_utils_test.h"
 
@@ -39,7 +38,6 @@ static suite_creator_t suites[] =
         gu_time_suite,
         gu_fifo_suite,
         gu_uuid_suite,
-        gu_lock_step_suite,
         gu_str_suite,
         gu_utils_suite,
         NULL
